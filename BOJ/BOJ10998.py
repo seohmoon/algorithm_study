c36feda8be9496dcceb187b70c06c13c5f565c61
seohmoon <dream222ing@gmail.com>
2022-02-20 23:54:14 +0900

    20220220

---
+++ BOJ/BOJ10998.py
@@ -0,0 +1,3 @@
+#BOJ10998 
+A, B = map(int, input().split())
+print(A*B)
---
+++ BOJ/BOJ2805.py
@@ -0,0 +1,13 @@
+# BOJ 2805 나무 자르기
+N, M = map(int, input().split())
+trees = list(map(int, input().split()))
+trees.sort()
+
+for i in range(trees[-2]+1,-1,-1):
+    tem = 0
+    for j in trees:
+        if j > i:
+            tem += j - i
+    if tem >= M:
+        print(i)
+        break
