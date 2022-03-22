3eef710077598a918cfbef1bca9b3170d94bf09e
seohmoon <dream222ing@gmail.com>
2022-03-22 23:57:26 +0900

    20220322

--- BOJ/BOJ11659.py
+++ BOJ/BOJ11659.py
@@ -1,5 +1,27 @@
 # BOJ11659 구간 합 구하기
+# 투포인터로 풀어도 되고, 누적합으로 풀어도 됩니당
+
+# 누적합
+from sys import prefix
+
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+prefix = [0 for _ in range(N+1)] # N+1인 이유는 앞에 패딩
+
+for i in range(1, N+1):
+    prefix[i] = prefix[i - 1] + arr[1]
+
+for j in range(M):
+    a, b = map(int, input().split())
+
+    print(prefix[b] - prefix[a - 1])
+
+
+
+'''
 import sys
+얘는 시간초과ㅠ
 
 N, M = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
@@ -12,3 +34,4 @@ for a in range(M):
         for b in range(i-1, j):
             total += arr[b]
     print(total)
+    '''
