5da9e647573f9614889e6367e532ca331ff66277
seohmoon <dream222ing@gmail.com>
2022-04-15 06:39:01 +0900

    20220415

--- BOJ/BOJ2581.py
+++ BOJ/BOJ2581.py
@@ -1,23 +1,21 @@
 # BOJ 2581 소수
-def sosoo(x):
-    global all
-    global minnum
-    for i in range(2, x):
-        if x % i == 0:
-            return False
-    all += x
-    if minnum == 0:
-        minnum = x
-    return True
-
 M = int(input())
 N = int(input())
 all = 0
 minnum = 0
 for i in range(M, N+1):
-    sosoo(i)
+    pn = True
+    for j in range(2, i):
+        if i % j == 0:
+            pn = False
+            break
+    if pn:
+        if minnum == 0:
+            minnum = i
+        all += i
+
 if all == 0:
-    print("-1")
+    print(-1)
 else:
     print(all)
-    print(minnum)
+    print(minnum)   
