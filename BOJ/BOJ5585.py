b62f366333ce60c2a1c52c6489e3521479c58ebe
seohmoon <dream222ing@gmail.com>
2022-04-28 00:14:22 +0900

    BOJ5585

--- BOJ/BOJ5585.py
+++ BOJ/BOJ5585.py
@@ -1,8 +1,13 @@
 # BOJ5585 거스름돈
 N = int(input())
+change = 1000 - N
 ans = 0
 # 500, 100, 50, 10, 5, 1 중 거스름돈 갯수 가장 적게
 coins = [500, 100, 50, 10, 5, 1]
 for coin in coins:
-    if coin <= N:
-        
+    if coin <= change:
+        ans += change // coin
+        change = change % coin
+        if change == 0:
+            break
+print(ans)
