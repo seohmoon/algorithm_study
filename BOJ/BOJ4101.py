79b272d791a3a5fcb50a7e7f1daf7d7f5bdb82f2
seohmoon <dream222ing@gmail.com>
2022-05-30 14:57:57 +0900

    20220530

---
+++ BOJ/BOJ4101.py
@@ -0,0 +1,10 @@
+# BOJ4101 크냐?
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0 :
+        break
+    else :
+        if a > b:
+            print("Yes")
+        elif a <= b:
+            print("No")
