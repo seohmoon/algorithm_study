d90786c9a01fb99728949936cab2df1c443be7e0
seohmoon <dream222ing@gmail.com>
2022-06-26 00:02:47 +0900

    20220626

--- BOJ/BOJ10953.py
+++ BOJ/BOJ10953.py
@@ -1,7 +1,7 @@
 # BOJ10953 
 T = int(input())
 for i in range(T):
-    lst = list(map(str, input().split()))
-    a = int(lst[0])
-    b = int(lst[-1])
+    arr = input()
+    a = int(arr[0])
+    b = int(arr[-1])
     print(a+b)
